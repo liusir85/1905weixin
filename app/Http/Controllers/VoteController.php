c3bcbee53fa26c7b158d474e7c31fea9c06cef99
liuqingyuan <liuqinyuan@qq.com>
2019-12-19 08:00:07 +0800

    xiugai

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -28,7 +28,7 @@ class VoteController extends Controller
         if(Redis::zrank($key,$user_info['openid'])){
             echo "已经投过票了";
         }else{
-            Redis::Zadd($key,time(),$openid);
+            Redis::zadd($key,time(),$openid);
         }
 
 
@@ -36,7 +36,7 @@ class VoteController extends Controller
         $total = Redis::zCard($key);        // 获取总数
         echo '投票总人数： '.$total;echo '</br>';
         $members = Redis::zRange($key,0,-1,true);       // 获取所有投票人的openid
-        echo '<pre>';print_r($members);echo '</pre>';
+//        echo '<pre>';print_r($members);echo '</pre>';
         foreach($members as $k=>$v){
             echo "用户： ".$k . ' 投票时间: '. date('Y-m-d H:i:s',$v);echo '</br>';
         }
@@ -53,16 +53,16 @@ class VoteController extends Controller
     }
 
     //根据code获取access_token $code
-    protected function getAccessToken($code){
+    public function getAccessToken($code){
         $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
         $json_data=file_get_contents($url);
-        return json_decode($json_data,true);
+        return $data=json_decode($json_data,true);
 
 //        echo '<pre>';print_r($data);echo'</pre>';
     }
 
     //获取用户的基本信息
-    protected function getUserInfo($access_token,$openid){
+    public function getUserInfo($access_token,$openid){
         $url='https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
         $json_data=file_get_contents($url);
        $data=json_decode($json_data,true);
